fd980e27f53f8860dfb1a91f4d1c9ff20c14cfdc
Thiyagarajan B <thyagubala@gmail.com>
2019-05-26 23:20:49 +0530

    Basic spring boot app code

---
+++ src/main/java/com/mydemo/app/exampleapp/ExampleAppApplication.java
@@ -0,0 +1,12 @@
+package com.mydemo.app.exampleapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExampleAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExampleAppApplication.class, args);
+	}
+}
---
+++ src/main/java/com/mydemo/app/exampleapp/GreetingController.java
@@ -0,0 +1,19 @@
+package com.mydemo.app.exampleapp;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+class GreetingController {
+
+    @RequestMapping("/hello/{name}")
+    String hello(@PathVariable String name) {
+        return "Hello there, " + name + "!";
+    }
+
+    @RequestMapping("/get")
+    public String index() {
+        return "Greetings from Application!";
+    }
+}
