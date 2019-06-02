c3e5bda6c9149afd2c62647d9cc7fd5edf4fd63b
Thiyagarajan B <thyagubala@gmail.com>
2019-06-02 14:33:18 +0530

    Changing port and enabling CORS

--- src/main/java/com/mydemo/app/exampleapp/GreetingController.java
+++ src/main/java/com/mydemo/app/exampleapp/GreetingController.java
@@ -1,5 +1,6 @@
 package com.mydemo.app.exampleapp;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,11 +9,13 @@ import org.springframework.web.bind.annotation.RestController;
 class GreetingController {
 
     @RequestMapping("/hello/{name}")
+    @CrossOrigin
     String hello(@PathVariable String name) {
         return "Hello there, " + name + "!";
     }
 
     @RequestMapping("/get")
+    @CrossOrigin
     public String index() {
         return "Greetings from the Application..!";
     }
